ea250b9bf1f9f02c6e63f1080da7f61268c89e39
sergeydomanov <nomad-labs@yandex.ru>
2022-11-28 16:16:12 +0300

    first commit

---
+++ src/main/java/com/nomad/algorithm/AlgorithmPart1Application.java
@@ -0,0 +1,13 @@
+package com.nomad.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmPart1Application.class, args);
+    }
+
+}
